ac1ea61b240ba7f4cd3f3fdfb24c8f9f56fbe2d5
Nasim <mdnasimhosen333@gmail.com>
2025-05-25 20:59:16 +0600

    Update output values in output.txt and print.cpp for consistency

--- print.cpp
+++ print.cpp
@@ -3,10 +3,12 @@
 int main()
 {
     std ::cout << "Hello World! \n";
-    int x = 10;
+    int x = 100;
     char c = 'A';
     double d = 3.1414;
     std::cout << x << std::endl
               << c << " " << d << std::endl;
+    // My Favorite number is 100
+    std::cout << "My Favorite number is " << x << std::endl;
     return 0;
 }
