f1b25e38ad7dc1824c4c2f3a3d5f8654d01a70bb
Nasim <mdnasimhosen333@gmail.com>
2025-05-25 23:22:33 +0600

    Update input and output files; enhance min/max swap functionality

--- min_max_swap.cpp
+++ min_max_swap.cpp
@@ -8,5 +8,9 @@ int main()
     cin >> v >> x >> y >> z;
     cout << min({v, x, y, z}) << endl;
     cout << max({v, x, y, z}) << endl;
+    int a, b;
+    cin >> a >> b;
+    swap(a, b);
+    cout << a << " " << b << endl;
     return 0;
 }
