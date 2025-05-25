edb1e78bda4eb9fcdc06369a548f8df97773924b
Nasim <mdnasimhosen333@gmail.com>
2025-05-25 21:09:37 +0600

    Add input handling for character and update output formatting

--- input.cpp
+++ input.cpp
@@ -4,7 +4,10 @@ using namespace std;
 int main()
 {
     int x;
+    char c;
     cin >> x;
+    cin >> c;
     cout << "You entered: " << x << endl;
+    cout << "You entered: " << c << endl;
     return 0;
 }
