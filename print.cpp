3baa4cec88fbac6e35a5778c9376c98bf6134f4d
Nasim <mdnasimhosen333@gmail.com>
2025-05-25 20:51:45 +0600

    Update output.txt and print.cpp with output values and formatting

--- print.cpp
+++ print.cpp
@@ -2,6 +2,11 @@
 
 int main()
 {
-    std ::cout << "Hello World!";
+    std ::cout << "Hello World! \n";
+    int x = 10;
+    char c = 'A';
+    double d = 3.1414;
+    std::cout << x << std::endl
+              << c << " " << d << std::endl;
     return 0;
 }
