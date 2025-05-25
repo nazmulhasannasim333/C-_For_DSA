2df25cf01131b544bd79d11d03eeee4cec13b929
Nasim <mdnasimhosen333@gmail.com>
2025-05-25 21:32:15 +0600

    Refactor code structure for improved readability and maintainability

---
+++ if_else.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 3;
+    if (x % 2 == 0)
+    {
+        cout << "EVEN" << endl;
+    }
+    else
+    {
+        cout << "ODD" << endl;
+    }
+}
